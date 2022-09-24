e8db785f05f9a1ce0ed17527fb4aadb62b8e8cfe
Hibikeyuri <hibikeyuri@gmail.com>
2022-09-25 00:24:59 +0800

    finish Lab util: pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -8,30 +8,71 @@ main(int argc, char *argv[])
     int fd_parent_to_child[2];
     int fd_child_to_parent[2];
 
-    pipe(&fd_child_to_parent);
-    pipe(&fd_parent_to_child);
+    pipe(fd_child_to_parent);
+    pipe(fd_parent_to_child);
 
     int pid = fork();
     int status = 0;
 
-    char buf_char = "a";
+    char buf_char = 'A';
 
     if(pid < 0)
     {
-        fprintf(2, "something error happend!\n");
+        fprintf(2, "pid < 0, error!\n");
         close(fd_child_to_parent[0]);
         close(fd_child_to_parent[1]);
         close(fd_parent_to_child[0]);
         close(fd_parent_to_child[1]);
-        exit(1);
+        status = 1;
     }
     else if (pid == 0)
     {
-        
+        /*read one character from parent process*/
+        if(read(fd_parent_to_child[0], &buf_char, sizeof(buf_char)) != sizeof(buf_char))
+        {
+            printf("read from parent error!\n");
+            status = 1;
+        }
+        else
+        {
+            printf("%d: received ping\n", getpid());
+            status = 0;
+        }
+
+        if(write(fd_child_to_parent[1], &buf_char, sizeof(buf_char)) != sizeof(buf_char))
+        {
+            printf("write to parent error!\n");
+            status = 1;
+        }
+        else
+        {
+            status = 0;
+        }
     }
     else
     {
+         /*read one character from parent process*/
+        if(write(fd_parent_to_child[1], &buf_char, sizeof(buf_char)) != sizeof(buf_char))
+        {
+            printf("write to child error!\n");
+            status = 1;
+        }
+        else
+        {
+            status = 0;
+        }
 
+        if(read(fd_child_to_parent[0], &buf_char, sizeof(buf_char)) != sizeof(buf_char))
+        {
+            printf("read from child error!\n");
+            status = 1;
+        }
+        else
+        {
+            printf("%d: received pong\n", getpid());
+            status = 0;
+        }       
     }
-    
+
+    exit(status);
 }
